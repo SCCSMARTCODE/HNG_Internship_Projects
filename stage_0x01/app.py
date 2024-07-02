fd576504552c9bcdba0ba2d08701c4d893dcd8ff
SCCSMARTCODE <sccsmart247@gmail.com>
2024-07-02 13:46:31 +0100

    updated to avoid hardcoding

--- stage_0x01/app.py
+++ stage_0x01/app.py
@@ -1,29 +1,33 @@
 from flask import Flask, request, jsonify
 import requests
 import json
+import os
 
 app = Flask('HGN Project1')
-app.config['MY_GEOLOCATION_KEY'] = '2b11e989f2254cbe9b8de6c751514a5e'
-app.config['MY_WEATHER_KEY'] = 'f7l11gptamt54sc6gdg9cmtqxh2t9ro571a4uced'
+app.config['MY_GEOLOCATION_KEY'] = os.getenv('MY_GEOLOCATION_KEY')
+app.config['MY_WEATHER_KEY'] = os.getenv('MY_WEATHER_KEY')
 
 
 @app.route('/api/hello')
 def index():
-    visitor_name = request.args.get('visitor_name').strip('"') or 'Anonymous'
+    try:
+        visitor_name = request.args.get('visitor_name', 'Anonymous').strip('"')
+    except Exception as e:
+        visitor_name = 'Anonymous'
     geo_location = get_geolocation()
     weather = get_weather_temperature(geo_location['longitude'], geo_location['latitude'])
 
     output = {
-         "client_ip": f"{geo_location['ip_address']}",
-          "location": f"{geo_location['city']}",
-          "greeting": f"Hello, {visitor_name}!, the temperature is {weather['current']['temperature']} degrees Celcius in {geo_location['city']}"
+        "client_ip": geo_location['ip_address'],
+        "location": geo_location['city'],
+        "greeting": f"Hello, {visitor_name}!, the temperature is {weather['current']['temperature']} degrees Celsius in {geo_location['city']}"
     }
     print(output)
     return jsonify(output)
 
 
 def get_geolocation():
-    request_url = 'https://ipgeolocation.abstractapi.com/v1/?api_key=' + app.config['MY_GEOLOCATION_KEY']
+    request_url = f'https://ipgeolocation.abstractapi.com/v1/?api_key={app.config["MY_GEOLOCATION_KEY"]}'
     response = requests.get(request_url)
     result = json.loads(response.content)
     return result
