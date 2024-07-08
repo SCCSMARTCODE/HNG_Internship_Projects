00ad3ddb15e482064f7ed706b59b3254d24a2a81
SCCSMARTCODE <sccsmart247@gmail.com>
2024-07-08 20:18:11 +0100

    updated

--- stage_0x02/database_setup.sql
+++
@@ -1,6 +0,0 @@
--- This will help us set up the database
-
-CREATE DATABASE IF NOT EXISTS `hng_stage2_project_db`;
-CREATE USER IF NOT EXISTS `hng_db_user`@localhost IDENTIFIED BY '246c4fb2a74a5571b749191853aeb801';
-GRANT ALL PRIVILEGES ON hng_stage2_project_db.* TO 'hng_db_user'@'localhost';
-FLUSH PRIVILEGES;
